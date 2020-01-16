d61c976e20509cbda4d4bd927c91c44ef43d5c44
Harold Yepez <hyepez@espol.edu.ec>
2020-01-16 08:49:36 -0500

    coneccion con mysql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'djangodb',
+        'USER':'admin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
